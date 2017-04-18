dac8a5b865af1b4b04463d098918a0b70320c6e1
venkatmv <venkatesh@mv2.com>
2017-04-17 20:49:24 -0700

    Add files via upload

---
+++ minisat/core/depend.mk
@@ -0,0 +1,27 @@
+/Users/madrasvenkatesh/Documents/minisat/core/Main.o /Users/madrasvenkatesh/Documents/minisat/core/Main.or /Users/madrasvenkatesh/Documents/minisat/core/Main.od /Users/madrasvenkatesh/Documents/minisat/core/Main.op: /Users/madrasvenkatesh/Documents/minisat/core/Main.cc \
+  /Users/madrasvenkatesh/Documents/minisat/utils/System.h \
+  /Users/madrasvenkatesh/Documents/minisat/mtl/IntTypes.h \
+  /Users/madrasvenkatesh/Documents/minisat/utils/ParseUtils.h \
+  /Users/madrasvenkatesh/Documents/minisat/utils/Options.h \
+  /Users/madrasvenkatesh/Documents/minisat/mtl/Vec.h \
+  /Users/madrasvenkatesh/Documents/minisat/mtl/XAlloc.h \
+  /Users/madrasvenkatesh/Documents/minisat/core/Dimacs.h \
+  /Users/madrasvenkatesh/Documents/minisat/core/SolverTypes.h \
+  /Users/madrasvenkatesh/Documents/minisat/mtl/Alg.h \
+  /Users/madrasvenkatesh/Documents/minisat/mtl/Map.h \
+  /Users/madrasvenkatesh/Documents/minisat/mtl/Alloc.h \
+  /Users/madrasvenkatesh/Documents/minisat/core/Solver.h \
+  /Users/madrasvenkatesh/Documents/minisat/mtl/Heap.h
+/Users/madrasvenkatesh/Documents/minisat/core/Solver.o /Users/madrasvenkatesh/Documents/minisat/core/Solver.or /Users/madrasvenkatesh/Documents/minisat/core/Solver.od /Users/madrasvenkatesh/Documents/minisat/core/Solver.op: /Users/madrasvenkatesh/Documents/minisat/core/Solver.cc \
+  /Users/madrasvenkatesh/Documents/minisat/mtl/Sort.h \
+  /Users/madrasvenkatesh/Documents/minisat/mtl/Vec.h \
+  /Users/madrasvenkatesh/Documents/minisat/mtl/IntTypes.h \
+  /Users/madrasvenkatesh/Documents/minisat/mtl/XAlloc.h \
+  /Users/madrasvenkatesh/Documents/minisat/core/Solver.h \
+  /Users/madrasvenkatesh/Documents/minisat/mtl/Heap.h \
+  /Users/madrasvenkatesh/Documents/minisat/mtl/Alg.h \
+  /Users/madrasvenkatesh/Documents/minisat/utils/Options.h \
+  /Users/madrasvenkatesh/Documents/minisat/utils/ParseUtils.h \
+  /Users/madrasvenkatesh/Documents/minisat/core/SolverTypes.h \
+  /Users/madrasvenkatesh/Documents/minisat/mtl/Map.h \
+  /Users/madrasvenkatesh/Documents/minisat/mtl/Alloc.h
---
+++ minisat/simp/depend.mk
@@ -0,0 +1,59 @@
+/Users/madrasvenkatesh/Documents/minisat/simp/Main.o /Users/madrasvenkatesh/Documents/minisat/simp/Main.or /Users/madrasvenkatesh/Documents/minisat/simp/Main.od /Users/madrasvenkatesh/Documents/minisat/simp/Main.op: /Users/madrasvenkatesh/Documents/minisat/simp/Main.cc \
+  /Users/madrasvenkatesh/Documents/minisat/utils/System.h \
+  /Users/madrasvenkatesh/Documents/minisat/mtl/IntTypes.h \
+  /Users/madrasvenkatesh/Documents/minisat/utils/ParseUtils.h \
+  /Users/madrasvenkatesh/Documents/minisat/utils/Options.h \
+  /Users/madrasvenkatesh/Documents/minisat/mtl/Vec.h \
+  /Users/madrasvenkatesh/Documents/minisat/mtl/XAlloc.h \
+  /Users/madrasvenkatesh/Documents/minisat/core/Dimacs.h \
+  /Users/madrasvenkatesh/Documents/minisat/core/SolverTypes.h \
+  /Users/madrasvenkatesh/Documents/minisat/mtl/Alg.h \
+  /Users/madrasvenkatesh/Documents/minisat/mtl/Map.h \
+  /Users/madrasvenkatesh/Documents/minisat/mtl/Alloc.h \
+  /Users/madrasvenkatesh/Documents/minisat/simp/SimpSolver.h \
+  /Users/madrasvenkatesh/Documents/minisat/mtl/Queue.h \
+  /Users/madrasvenkatesh/Documents/minisat/core/Solver.h \
+  /Users/madrasvenkatesh/Documents/minisat/mtl/Heap.h
+/Users/madrasvenkatesh/Documents/minisat/simp/SimpSolver.o /Users/madrasvenkatesh/Documents/minisat/simp/SimpSolver.or /Users/madrasvenkatesh/Documents/minisat/simp/SimpSolver.od /Users/madrasvenkatesh/Documents/minisat/simp/SimpSolver.op: /Users/madrasvenkatesh/Documents/minisat/simp/SimpSolver.cc \
+  /Users/madrasvenkatesh/Documents/minisat/mtl/Sort.h \
+  /Users/madrasvenkatesh/Documents/minisat/mtl/Vec.h \
+  /Users/madrasvenkatesh/Documents/minisat/mtl/IntTypes.h \
+  /Users/madrasvenkatesh/Documents/minisat/mtl/XAlloc.h \
+  /Users/madrasvenkatesh/Documents/minisat/simp/SimpSolver.h \
+  /Users/madrasvenkatesh/Documents/minisat/mtl/Queue.h \
+  /Users/madrasvenkatesh/Documents/minisat/core/Solver.h \
+  /Users/madrasvenkatesh/Documents/minisat/mtl/Heap.h \
+  /Users/madrasvenkatesh/Documents/minisat/mtl/Alg.h \
+  /Users/madrasvenkatesh/Documents/minisat/utils/Options.h \
+  /Users/madrasvenkatesh/Documents/minisat/utils/ParseUtils.h \
+  /Users/madrasvenkatesh/Documents/minisat/core/SolverTypes.h \
+  /Users/madrasvenkatesh/Documents/minisat/mtl/Map.h \
+  /Users/madrasvenkatesh/Documents/minisat/mtl/Alloc.h \
+  /Users/madrasvenkatesh/Documents/minisat/utils/System.h
+/Users/madrasvenkatesh/Documents/minisat/core/Main.o /Users/madrasvenkatesh/Documents/minisat/core/Main.or /Users/madrasvenkatesh/Documents/minisat/core/Main.od /Users/madrasvenkatesh/Documents/minisat/core/Main.op: /Users/madrasvenkatesh/Documents/minisat/core/Main.cc \
+  /Users/madrasvenkatesh/Documents/minisat/utils/System.h \
+  /Users/madrasvenkatesh/Documents/minisat/mtl/IntTypes.h \
+  /Users/madrasvenkatesh/Documents/minisat/utils/ParseUtils.h \
+  /Users/madrasvenkatesh/Documents/minisat/utils/Options.h \
+  /Users/madrasvenkatesh/Documents/minisat/mtl/Vec.h \
+  /Users/madrasvenkatesh/Documents/minisat/mtl/XAlloc.h \
+  /Users/madrasvenkatesh/Documents/minisat/core/Dimacs.h \
+  /Users/madrasvenkatesh/Documents/minisat/core/SolverTypes.h \
+  /Users/madrasvenkatesh/Documents/minisat/mtl/Alg.h \
+  /Users/madrasvenkatesh/Documents/minisat/mtl/Map.h \
+  /Users/madrasvenkatesh/Documents/minisat/mtl/Alloc.h \
+  /Users/madrasvenkatesh/Documents/minisat/core/Solver.h \
+  /Users/madrasvenkatesh/Documents/minisat/mtl/Heap.h
+/Users/madrasvenkatesh/Documents/minisat/core/Solver.o /Users/madrasvenkatesh/Documents/minisat/core/Solver.or /Users/madrasvenkatesh/Documents/minisat/core/Solver.od /Users/madrasvenkatesh/Documents/minisat/core/Solver.op: /Users/madrasvenkatesh/Documents/minisat/core/Solver.cc \
+  /Users/madrasvenkatesh/Documents/minisat/mtl/Sort.h \
+  /Users/madrasvenkatesh/Documents/minisat/mtl/Vec.h \
+  /Users/madrasvenkatesh/Documents/minisat/mtl/IntTypes.h \
+  /Users/madrasvenkatesh/Documents/minisat/mtl/XAlloc.h \
+  /Users/madrasvenkatesh/Documents/minisat/core/Solver.h \
+  /Users/madrasvenkatesh/Documents/minisat/mtl/Heap.h \
+  /Users/madrasvenkatesh/Documents/minisat/mtl/Alg.h \
+  /Users/madrasvenkatesh/Documents/minisat/utils/Options.h \
+  /Users/madrasvenkatesh/Documents/minisat/utils/ParseUtils.h \
+  /Users/madrasvenkatesh/Documents/minisat/core/SolverTypes.h \
+  /Users/madrasvenkatesh/Documents/minisat/mtl/Map.h \
+  /Users/madrasvenkatesh/Documents/minisat/mtl/Alloc.h
